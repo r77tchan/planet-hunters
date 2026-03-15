b2d1bf0d13cca8a7535f0ae8f11e0667995e62d7
r77tchan <r77tchan@gmail.com>
2026-03-16 00:10:34 +0900

    余白調整

--- components/AreYouReady.tsx
+++ components/AreYouReady.tsx
@@ -2,7 +2,7 @@ import PlayNowButton from "./PlayNowButton";
 
 export default function AreYouReady() {
   return (
-    <div className="py-17.5 text-center md:py-25">
+    <div className="mx-auto max-w-292.5 px-5 py-17.5 text-center md:px-7.5 md:py-25">
       <div className="mb-5 text-[32px] font-bold tracking-wider text-white md:mb-6.25 md:text-[64px]">
         ARE YOU READY TO FIGHT?
       </div>
